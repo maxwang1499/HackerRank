774a88e919cd2978f9f405eecc74b3601dbcb4bc
maxwang1499 <85645670+maxwang1499@users.noreply.github.com>
2022-06-01 12:21:00 -0400

    Create arithmetic_operators.py

---
+++ Introduction/arithmetic_operators.py
@@ -0,0 +1,17 @@
+'''
+Task: The input reads two integers from STDIN, a and b. Add code to print three lines where:
+
+    The first line contains the sum of the two numbers.
+    The second line contains the difference of the two numbers (first - second).
+    The third line contains the product of the two numbers.
+
+'''
+#Input
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    
+#Code    
+    print (a+b)
+    print (a-b)
+    print (a*b)
